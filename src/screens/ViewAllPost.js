c1d742f61218995958ff4023933ecdce75c2ee9a
loganpmahan <loganpearcemahan@gmail.com>
2019-03-26 18:08:28 -0500

    Adding the rest of the files

---
+++ src/screens/ViewAllPost.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableHighlight } from 'react-native';
+import PostComponent from '../components/PostComponent';
+
+import { db } from '../config';
+import firebase from 'firebase';
+let itemsRef = db.ref('/post');
+let addItem = (description, date, picture) => {
+let valuekey = db.ref('/post').push().key;
+
+  db.ref('/post/' + valuekey).update({
+    description: description,
+    date: new Date(),
+    picture: picture,
+    user: firebase.auth().currentUser.uid,
+    key: valuekey,
+
+
+  });
+};
+export default class List extends Component {
+  state = {
+    items: [],
+    description: '',
+    date: '',
+    picture: '',
+    key: '',
+  };
+
+  handleSubmit = () => {
+    addItem(
+      this.state.description,
+      this.state.date,
+      this.state.picture,
+    );
+    alert('Success');
+  };
+
+  componentDidMount() {
+    itemsRef.on('value', snapshot => {
+      let data = snapshot.val();
+      let items = Object.values(data);
+      this.setState({ items });
+      //}
+    });
+  }
+
+  render() {
+    return (
+
+      <View>
+        <Text> Archives </Text>
+        <TextInput style={styles.itemInput} onChangeText={description => this.setState({ description })} />
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor="white"
+          onPress={this.handleSubmit}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableHighlight>
+        {this.state.items.length > 0 ? (
+          <PostComponent items={this.state.items} />
+          ) : (
+          <Text>No items</Text>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ebebeb'
+  }
+});
