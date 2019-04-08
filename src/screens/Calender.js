035d59a10e8e6d9e42b8a215754647c36ac8eae9
sovankosal <sly5@wisc.edu>
2019-04-07 20:02:03 -0500

    Remove unused files, fixed Calendar bugs + update

--- src/screens/Calender.js
+++ src/screens/Calender.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-
 import {CalendarList} from 'react-native-calendars';
 import {View, Modal, Text, TouchableHighlight, Button} from 'react-native';
-import List from './ListEvent'
+import List from './SelectedDayEvent'
 import { db } from '../config';
 
 
@@ -31,6 +30,8 @@ export default class HorizontalCalendarList extends Component {
       currentUserUid: '',
       full_Name: '',
       attendantNum: '',
+      date: '',
+      currentDate: '',
     };
   }
 
@@ -45,7 +46,7 @@ export default class HorizontalCalendarList extends Component {
       full_Name: this.props.navigation.state.params.full_Name,
       attendantNum: this.props.navigation.state.params.attendantNum,
     });
-
+    this.getCurrentDate();
     this.anotherFunc();
     }
 
@@ -54,34 +55,56 @@ export default class HorizontalCalendarList extends Component {
     var obj = nextDay.reduce((c, v) => Object.assign(c, {[v]: {selected: true}}), {});
     this.setState({ marked : obj});
 }
+
+    setDate = (day) => {
+      let days = day.dateString
+      this.setState({
+        date: days
+      });
+    }
+
+    getCurrentDate = () => {
+      let date = new Date();
+      let day;
+      let month;
+      //Get the day into correct format 01-09
+      if (date.getDate() < 10){
+        day = '0' + date.getDate();
+      }
+      else {
+        day = date.getDate();
+      }
+
+      // date.getMonth return 0-11
+      if ((date.getMonth() + 1) < 10){
+        month = '0' + (date.getMonth() + 1);
+      }
+      else {
+        month = date.getMonth() + 1;
+      }
+
+      let year = date.getFullYear();
+      //date format YYYY-MM-DD
+      let eventDate = year+'-'+month+'-'+day;
+       this.setState({
+         currentDate: eventDate
+       });
+    }
   render() {
     return (
       <View>
         <CalendarList
-          current={'2019-03-16'}
+          current= {this.state.currentDate}
           pastScrollRange={24}
           futureScrollRange={24}
           horizontal
           pagingEnabled
           style={{borderBottomWidth: 1, borderBottomColor: 'black'}}
-          hideArrows={false}
-          hideDayNames={true}
-          renderArrow={(direction) => (<Arrow />)}
-          onDayPress={(day) => { this.props.navigation.navigate('EventDetail',{
-                  date: day.dateString,
-                  email: this.props.navigation.state.params.email,
-                  first_Name: this.props.navigation.state.params.first_Name,
-                  last_Name: this.props.navigation.state.params.last_Name,
-                  role: this.props.navigation.state.params.role,
-                  currentUserUid: this.props.navigation.state.params.currentUserUid,
-                  full_Name:this.props.navigation.state.params.full_Name,
-                  attendantNum: this.props.navigation.state.params.attendantNum,
-                });  }}
-
+          hideDayNames={false}
+          onDayPress={(day) => this.setDate(day)}
           markedDates={this.state.marked}
         />
-        <List>
-        </List>
+        <List eventToday={this.state.date} navigation={this.props.navigation}/>
       </View>
     );
   }
